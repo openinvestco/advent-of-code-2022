8272289ff25751e2d2efd8fe53357a1e40c03527
Liam Foster <liamfoster@liamfostersmbp.home>
2022-12-02 23:45:46 +0000

    Finished go day two part two

--- liam/day_two/go/main.go
+++ liam/day_two/go/main.go
@@ -36,5 +36,28 @@ func partOne() {
 }
 
 func partTwo() {
+	dat, err := os.ReadFile("../input.txt")
+	check(err)
+	games := strings.Split(string(dat), "\n")
+	score := 0
+	for _, game := range games {
+		var move int
+		switch game[2] {
+		case 'Y':
+			tie_points := []int{1, 2, 3}
+			move = tie_points[int(game[0])-65]
+			score += move + 3 // Tie
+		case 'Z':
+			win_points := []int{2, 3, 1}
+			move = win_points[int(game[0])-65]
+			score += move + 6 // Win
+		default:
+			lose_points := []int{3, 1, 2}
+			move = lose_points[int(game[0])-65]
+			score += move // Lose
+		}
+
+	}
+	fmt.Printf("Total Score: %d\n", score)
 
 }
