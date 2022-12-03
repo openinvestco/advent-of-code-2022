77c108ed9a36de51386f7e3d6cb19f475c81a065
Liam Foster <liamfoster@liamfostersmbp.home>
2022-12-03 15:41:24 +0000

    Finished day three part one in bash

--- liam/day_three/bash/main.sh
+++ liam/day_three/bash/main.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
-input="../test_input.txt"
-while read -r line
-do 
-    echo "$line"
-done < "$input"
+
+part_one() {
+    input="../test_input.txt"
+    total=0
+    for line in $(cat $input)
+    do 
+        half=$((`echo "$line" | tr -d "\n" | wc -c` /2));
+        c=0
+        for (( i=0; i<${half}; i++ ))
+        do
+            if [[ "${line:half:half}" == *"${line:i:1}"* ]] 
+            then
+                c="${line:i:1}"
+            fi
+        done
+        x=$((`echo {"!",{a..z},{A..Z}} | tr -d "[:space:]" | grep -b -o $c | cut -d: -f1`))
+        total=$((total + $x))
+    done < "$input"
+    echo $total
+}
+part_one
