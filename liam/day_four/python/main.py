5b35c8024f1cf5432af7e22edff5d434dbfa74ab
Liam Foster <liamfoster@liamfostersmbp.home>
2022-12-04 16:52:13 +0000

    Made code a bit DRY-er

--- liam/day_four/python/main.py
+++ liam/day_four/python/main.py
@@ -4,27 +4,24 @@ def main():
     part_one()
     part_two()
 
-def part_one():
-    total_containing = 0
-    with open("../input.txt", "r", encoding = 'utf-8') as file:
+def loop_over_pairs(filename, closure):
+    total = 0
+    with open(filename, "r", encoding = 'utf-8') as file:
         for line in file:
             re_pair = re.compile(r'(\d+)-' + r'(\d+)')
             t1, t2 = re.findall(re_pair, line)
-            contains = lambda t1, t2 : int(t1[0]) <= int(t2[0]) and int(t1[1]) >= int(t2[1])
-            if contains(t1, t2) or contains(t2, t1):
-                total_containing += 1
-    print(total_containing)       
+            if closure(t1, t2) or closure(t2, t1):
+                total += 1
+    return total
+
+def part_one():
+    contains = lambda t1, t2 : int(t1[0]) <= int(t2[0]) and int(t1[1]) >= int(t2[1])
+    print(loop_over_pairs("../input.txt", contains))
     
 def part_two():
-    total_containing = 0
-    with open("../input.txt", "r", encoding = 'utf-8') as file:
-        for line in file:
-            re_pair = re.compile(r'(\d+)-' + r'(\d+)')
-            t1, t2 = re.findall(re_pair, line)
-            overlaps = lambda t1, t2 : int(t1[0]) <= int(t2[0]) and int(t1[1]) >= int(t2[0]) or int(t1[0]) <= int(t2[1]) and int(t1[1]) >= int(t2[1])
-            if overlaps(t1, t2) or overlaps(t2, t1):
-                total_containing += 1
-    print(total_containing)      
+    overlaps = lambda t1, t2 : int(t1[0]) <= int(t2[0]) and int(t1[1]) >= int(t2[0]) or int(t1[0]) <= int(t2[1]) and int(t1[1]) >= int(t2[1])
+    print(loop_over_pairs("../input.txt", overlaps))
+
 
 
 if __name__ == '__main__':
