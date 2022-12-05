775d147cf5bf8987210894c29649f14c1635f0cc
Liam Foster <liamfoster@liamfostersmbp.home>
2022-12-05 09:13:30 +0000

    Finished day five part one in rust

---
+++ liam/day_five/rust/main.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+
+// [V]         [T]         [J]        
+// [Q]         [M] [P]     [Q]     [J]
+// [W] [B]     [N] [Q]     [C]     [T]
+// [M] [C]     [F] [N]     [G] [W] [G]
+// [B] [W] [J] [H] [L]     [R] [B] [C]
+// [N] [R] [R] [W] [W] [W] [D] [N] [F]
+// [Z] [Z] [Q] [S] [F] [P] [B] [Q] [L]
+// [C] [H] [F] [Z] [G] [L] [V] [Z] [H]
+//  1   2   3   4   5   6   7   8   9 
+
+fn main() {
+    part_two();
+}
+
+fn part_one() {
+    let mut stacks = vec![
+        vec!["C", "Z", "N", "B", "M", "W", "Q", "V"], 
+        vec!["H", "Z", "R", "W", "C", "B"], 
+        vec!["F", "Q", "R", "J"], 
+        vec!["Z", "S", "W", "H", "F", "N", "M", "T"], 
+        vec!["G", "F", "W", "L", "N", "Q", "P"], 
+        vec!["L", "P", "W"], 
+        vec!["V", "B", "D", "R", "G", "C", "Q", "J"],
+        vec!["Z", "Q", "N", "B", "W"],
+        vec!["H", "L", "F", "C", "G", "T", "J"],
+    ];
+
+    println!("{:?}", stacks);
+    let input = fs::read_to_string("../input.txt")
+        .expect("Bad input, no presents for you");
+
+    let moves = input.split("\n");
+    for _move in moves {
+        let move_set= _move.split_whitespace().collect::<Vec<&str>>();
+        let count = move_set[1].parse::<i32>().unwrap();
+        let v1 = move_set[3].parse::<usize>().unwrap() -1 ;
+        let v2 = move_set[5].parse::<usize>().unwrap() -1;
+        for n in 0..count {
+            let buf = stacks[v1].pop().unwrap();
+            stacks[v2].push(buf);
+        }
+    }
+    for stack in stacks {
+        println!("{}", stack.last().unwrap())
+    }
+    
+}
