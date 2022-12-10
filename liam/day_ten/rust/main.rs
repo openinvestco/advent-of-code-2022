1c34af8d40377e767dd47b6aec44ac6df2fd986b
Liam Foster <liamfoster@liamfostersmbp.home>
2022-12-10 14:01:02 +0000

    Code made a bit more DRY and slightly optimized solution

--- liam/day_ten/rust/main.rs
+++ liam/day_ten/rust/main.rs
@@ -34,13 +34,14 @@ fn part_two() {
     let mut total_signal = 0;
     let mut crt_output: String = "".to_owned();
     for op in input.replace("\n", " ").split_whitespace() {
-        // println!("cycle: {cycle}, register: {register}");
         if ((register + 1) - (cycle % 40)).abs() <= 1 {
             crt_output.push_str("#")
         } else {
-            crt_output.push_str(".")
+            crt_output.push_str(" ")
         }
-        // println!("{crt_output}");
+        if cycle < 240 && cycle % 40 == 0 {
+            crt_output.push_str("\n")
+        }
         let parsed = op.parse::<i32>();
         match parsed {
             Ok(ok) => register += ok,
@@ -48,16 +49,6 @@ fn part_two() {
         }
         cycle += 1;
     }
-    let crt_wrapped = crt_output
-        .chars()
-        .enumerate()
-        .fold(String::new(), |acc, (i, c)| {
-            if i != 0 && i % 40 == 0 {
-                format!("{}\n{}", acc, c)
-            } else{
-                format!("{}{}", acc, c)
-            }
-        });
-    println!("{crt_wrapped}");
+    println!("{crt_output}");
 
 }
